580b82344bbb4341e8459e17fac2404729287916
Tatana <burmatova@inbox.ru>
2020-11-09 22:36:36 +0300

    Added methods

--- src/main/java/ru/netology/manager/IssueManager.java
+++ src/main/java/ru/netology/manager/IssueManager.java
@@ -1,14 +1,80 @@
 package ru.netology.manager;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import ru.netology.domain.Issue;
 import ru.netology.repository.IssueRepository;
+
+import java.util.*;
+import java.util.function.Predicate;
+
 
 public class IssueManager {
     private IssueRepository repository;
 
     public void saveIssue(Issue issue) {
+
         repository.addIssue(issue);
+    }
+
+    public List<Issue> filterByAuthor(String author) {
+        List<Issue> issuesByAuthor = new ArrayList<>();
+        Predicate<Issue> predicate = issue -> issue.getAuthor().equalsIgnoreCase(author);
+
+        for (Issue issue : repository.getAll()) {
+            if (predicate.test(issue)) {
+                issuesByAuthor.add(issue);
+            }
+        }
+        return issuesByAuthor;
+    }
+
+    public List<Issue> filterByLabel(Set<String> label) {
+        List<Issue> issuesByLabel = new ArrayList<>();
+        Predicate<Issue> predicate = issue -> issue.getLabel().equals(label);
+
+        for (Issue issue : repository.getAll()) {
+            if (predicate.test(issue)) {
+                issuesByLabel.add(issue);
+            }
+        }
+        return issuesByLabel;
+    }
+
+    public List<Issue> filterByAssignee(Set<String> assignee) {
+        List<Issue> issuesByAssignee = new ArrayList<>();
+        Predicate<Issue> predicate = issue -> issue.getAssignee().equals(assignee);
+
+        for (Issue issue : repository.getAll()) {
+            if (predicate.test(issue)) {
+                issuesByAssignee.add(issue);
+            }
+        }
+        return issuesByAssignee;
+    }
+
+    public List<Issue> sortByOldest(Comparator<Issue> comparator) {
+        List<Issue> oldestIssues = new ArrayList<>();
+
+        oldestIssues.addAll(repository.getAll());
+        oldestIssues.sort(comparator);
+        return oldestIssues;
+    }
+
+    public List<Issue> sortByNewest(Comparator<Issue> comparator) {
+        List<Issue> newestIssues = new ArrayList<>();
+
+        newestIssues.addAll(repository.getAll());
+        newestIssues.sort(comparator.reversed());
+        return newestIssues;
+    }
+
+//    прошу помочь разобраться с этим кодом
+    public boolean openIssueById(int id) {
+        for (Issue issue : repository.getAll()) {
+            if (issue.getId() == id && !issue.isOpen()) {
+               issue.setOpen(true);
+            }
+            return issue.isOpen();
+        }
+        return false;
     }
 }
